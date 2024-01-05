40db6c634bae73ee9733469ab76194fe8ce8f62c
sonyerg <ericksonrguinto@gmail.com>
2024-01-05 22:09:28 +1300

    changed width

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     >
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <a href="#">
-          <Image src={navLogo} width={70} height={50} />
+          <Image src={navLogo} width={50} />
         </a>
         <div>
           <ul className="hidden md:flex space-x-4 pr-4 text-gray-300">
